f2a2ac2dc50bdc16c2021c0513410990646a8ebf
LitenSnigel <43867457+LitenSnigel@users.noreply.github.com>
2019-02-12 11:16:56 +0100

    Create gurr.js

---
+++ gurr.js
@@ -0,0 +1,29 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  var y = document.getElementById("TB");
+  var z = document.getElementById("csgo");
+  y.style.display = "none";
+  z.style.display = "none";
+  x.style.display = "block";
+}
+
+function myFunction2() {
+  var x = document.getElementById("TB");
+  var y = document.getElementById("csgo");
+  var z = document.getElementById("myDIV");
+  y.style.display = "none";
+  z.style.display = "none";
+  x.style.display = "block";
+}
+
+
+function myFunction3() {
+  var x = document.getElementById("csgo");
+  var y = document.getElementById("TB");
+  var z = document.getElementById("myDIV");
+  y.style.display = "none";
+  z.style.display = "none";
+  x.style.display = "block";
+}
+
+
